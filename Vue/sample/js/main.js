289479ea6aa4c26c3e9b073ae63dad9503823ef9
MasaGt <39978196+MasaGt@users.noreply.github.com>
2018-12-21 14:12:01 +0900

    Day3 Conditional rendering

--- Vue/sample/js/main.js
+++ Vue/sample/js/main.js
@@ -3,8 +3,6 @@ var app = new Vue({
   data: {
     product: 'Socks',
     image: '../img/Socks.jpg',
-    link: 'https://www.google.co.jp/'
+    link: 'https://www.google.co.jp/',
   },
 });
-
-console.log(app);
