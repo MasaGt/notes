826aff8fb46558394e6ab35d8d06c36fdf5913d4
MasaGt <39978196+MasaGt@users.noreply.github.com>
2018-12-24 14:28:12 +0900

    Day5 Event handling

--- Vue/sample/js/main.js
+++ Vue/sample/js/main.js
@@ -2,9 +2,27 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    image: '../img/Socks.jpg',
+    image: '../img/Socks-green.jpg',
     link: 'https://www.google.co.jp/',
     stock: 11,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {id: 1, color: "Green", img:"../img/Socks-green.jpg"},
+      {id: 2, color: "Blue", img: "../img/Socks-blue.jpg"},
+    ],
+    cart: 0,
   },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart >= 1) {
+        this.cart -= 1;
+      }
+    },
+    changeImg(showImg) {
+      this.image = showImg;
+    }
+  }
 });
